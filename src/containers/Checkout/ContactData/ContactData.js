6d74a91cdde93422cb0a08a4c9ea5aa29befb781
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2019-02-11 15:45:24 -0800

    add form-validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,12 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -22,7 +27,12 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -30,7 +40,14 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'ZIP Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -38,7 +55,12 @@ class ContactData extends Component{
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -46,7 +68,12 @@ class ContactData extends Component{
                     type: 'email',
                     placeholder: 'Your Email'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -56,13 +83,16 @@ class ContactData extends Component{
                         {value: "cheapest", displayValue: "Cheapest"},
                         ]
                 },
-                value: ''
+                value: '',
+                validation: {},
+                valid: false
             }
-
             },
         loading: false,
-        price: 0
+        price: 0,
+        formisValid: false
     }
+
 
     orderHandler = (e) => {
         e.preventDefault();
@@ -87,6 +117,22 @@ class ContactData extends Component{
             this.setState({ loading: false })
         });
     }
+
+    checkValidaity = (value, rules) => {
+        let isValid = true;
+        if(rules) return true;
+        if (rules.required){
+            isValid = value.trim() !== '' && isValid;
+        }
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid;
+        }
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+        return isValid;
+    }
+
     inputChangedHandler = (e, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm,
@@ -94,10 +140,18 @@ class ContactData extends Component{
         const updatedFormElement = {
             ...updatedOrderForm[inputIdentifier]
         }
+        updatedFormElement.touched = true;
         updatedFormElement.value = e.target.value
+        updatedFormElement.valid = this.checkValidaity(updatedFormElement.value, updatedFormElement.validation)
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        
+        let formisValid= true;
+        for(let inputIdentifier in updatedOrderForm){
+            formisValid = updatedOrderForm[inputIdentifier].valid && formisValid
+        }
         this.setState({
-            orderForm: updatedOrderForm
+            orderForm: updatedOrderForm,
+            formisValid
         })
     }
 
@@ -117,10 +171,13 @@ class ContactData extends Component{
                         elementType={formElement.config.elementType} 
                         elementConfig={formElement.config.elementConfig} 
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(e) => this.inputChangedHandler(e, formElement.id)}
                         />
                 ))}
-                <Button btnType="Success">Order</Button>
+                <Button disabled={!this.state.formisValid} btnType="Success">Order</Button>
             </form> 
         );
 
--- src/hoc/WithErrorHandler/WithErrorHandler.js
+++ src/hoc/WithErrorHandler/WithErrorHandler.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Modal from "../../components/UI/Modal/Modal";
 import Aux from "../Aux/Aux";
-import Axios from "axios";
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
